82a277547e1ac10302e1e3b4dd9c8c2e3fb11eb7
ViottiViolet <viottiviolet@gmail.com>
2026-04-10 15:25:45 -0500

    added code for first room
    open to change in the future, especially pin assignments

--- SA2Enclosure_Code.ino
+++ SA2Enclosure_Code.ino
@@ -1,13 +1,21 @@
-//shadow the hedgehog's wild ride
+#include <Servo.h>
 
+Servo servo1;
+const int babyMariaPin = 9;
+const int servo1Pin = 13;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(babyMariaPin, INPUT);
+  servo1.attach(servo1Pin);
+  servo1.write(0); // resets testing tube to closed state. place baby shadow in AFTER
 
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // edit from my branch saharas branch i like metal csonic
+  Serial.println(digitalRead(babyMariaPin));
+  if (digitalRead(babyMariaPin) == HIGH)
+  {
+    servo1.write(180);
+  }
 }
