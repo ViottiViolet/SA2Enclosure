7ba5c6b4d391ed5e62889b62f16a4140aa4b6d4b
ViottiViolet <viottiviolet@gmail.com>
2026-04-15 15:44:05 -0500

    Update SA2Enclosure_Code.ino
    added functionality for eject room + reworked servo interactions. input pins -> input pullup

--- SA2Enclosure_Code.ino
+++ SA2Enclosure_Code.ino
@@ -1,21 +1,34 @@
 #include <Servo.h>
 
 Servo servo1;
-const int babyMariaPin = 9;
+Servo servo2;
+const int babyMariaPin = 12;
 const int servo1Pin = 13;
 
-void setup() {
-  pinMode(babyMariaPin, INPUT);
-  servo1.attach(servo1Pin);
-  servo1.write(0); // resets testing tube to closed state. place baby shadow in AFTER
+const int ejectButtonPin = 2;
+const int servo2Pin = 3;
 
+void setup() {
   Serial.begin(9600);
+  pinMode(babyMariaPin, INPUT_PULLUP);
+  pinMode(ejectButtonPin, INPUT_PULLUP);
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
+  servo1.write(0); // resets testing tube to closed state. place baby shadow in AFTER
+  servo2.write(0); // resets eject floor to closed state
+
+  
 }
 
 void loop() {
   Serial.println(digitalRead(babyMariaPin));
-  if (digitalRead(babyMariaPin) == HIGH)
+  if (digitalRead(ejectButtonPin) == LOW)
+  {
+    servo2.write(70);
+  }
+  if (digitalRead(babyMariaPin) == LOW)
   {
     servo1.write(180);
   }
+  //servo1.write(90);
 }
